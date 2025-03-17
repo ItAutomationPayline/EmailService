118f621a8efc3d9eaafa30d55308a9114028bbd9
ItAutomationPayline <it.automation@paylineindia.com>
2025-03-17 14:29:19 +0530

    NodeJs4

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const { sendEmail } = require('./emailService');
 const app = express();
 const PORT = 3000;
 
-
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(cors());
 
 // API Endpoint for sending emails
 app.post('/send-email', async (req, res) => {
